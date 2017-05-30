47f7850edcf21d3a98f0ce577d85af7c86c54dfd
Josh <josue0991@icloud.com>
2017-05-30 16:14:25 -0500

    url only

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ let url = require("url");
 
 var server = http.createServer(function(req, res) {
 	
-	let parsedUrl = url.parse(req.url);
-	res.end(parsedUrl);
+	
+	res.end(req.url);
 });
 
 server.listen(process.env.PORT || 8000, function() {
