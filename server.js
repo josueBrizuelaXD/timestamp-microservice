67f46c4ce713edf5753d527a66b3f7e182f16b4a
Josh <josue0991@icloud.com>
2017-06-23 15:14:29 -0500

    html page added

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
-var http = require("http");
-let url = require("url");
+const express = require("express");
+const app = express();
+const moment = require("moment");
+const fs = require("fs");
 
+let returnedObj = {};
 
-var server = http.createServer(function(req, res) {
-	
-	
-	res.end(req.url);
+app.get("/", function(req, res) {
+	 res.writeHead(200, {"Content-Type": "text/html"});
+	var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
+	myReadStream.pipe(res);
 });
 
-server.listen(process.env.PORT || 8000, function() {
-	console.log("listening to port 8000");
+
+app.get("/:input", function (req, res) {
+	const input = req.params.input;
+	
+	
+	const dateFromString = moment(input);
+	
+	if (!isNaN(input)) {
+		returnedObj = {"unix" : input, "natural" : moment.unix(input).utc().format("MMMM DD YYYY")};
+		
+		res.send(returnedObj);
+	}
+	
+	if (dateFromString.isValid()) {
+		returnedObj = {"unix" : dateFromString.unix(), "natural" : dateFromString.format("MMMM DD, YYYY")};
+		res.send(returnedObj);
+	} else {
+		returnedObj = {"unix" : null, "natural" : null};
+		res.send(returnedObj);
+	}
+	
 });
+
+
+
+
+app.listen(process.env.PORT);
